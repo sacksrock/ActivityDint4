1da3453e3c7991f8b5393ef70af8486f22b9b499
Daniel de la Iglesia Gonzalez <daniel.iglesia@live.u-tad.com>
2018-01-18 18:37:26 +0100

    Actividad 4 Dint -- imagen

--- app/src/main/java/com/utad/danieliglesia/actividad4interfaces/MainActivity.java
+++ app/src/main/java/com/utad/danieliglesia/actividad4interfaces/MainActivity.java
@@ -13,14 +13,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
